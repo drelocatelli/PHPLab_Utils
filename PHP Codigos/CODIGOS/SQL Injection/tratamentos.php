57977f794bf8356a82f7e1709ac8e933ed4b7bcb
Your Name <you@example.com>
2021-05-06 18:23:19 -0300

    ..

---
+++ PHP Codigos/CODIGOS/SQL Injection/tratamentos.php
@@ -0,0 +1,15 @@
+PREG_REPLACE:
+$_POST['login'] = preg_replace('/[^[:alpha:]_]/', '',$_POST['login']);
+
+
+OUTROS MEIOS:
+$email = stripslashes($_POST['email']);
+$email = strip_tags($email);
+$email = mysqli_real_escape_string($db_conx, $email);
+$password = preg_replace("[^A-Za-z0-9]", "", $_POST['password']);
+$password = md5($password);
+
+
+FILTER_VAR:
+filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)
+
