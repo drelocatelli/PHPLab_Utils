8c9d6b2419f32c98d17b6afdd256d465298a4660
Your Name <you@example.com>
2021-05-07 11:30:47 -0300

    ..

--- PHP Codigos/CODIGOS/SQL Injection/em tags html e textos.php
+++ PHP Codigos/CODIGOS/SQL Injection/em tags html e textos.php
@@ -1,5 +1,14 @@
-Anti Sqllnjection e tags html 
-PHP 
-function anti_injection(Ssql) { // remove palavras que contenham sintaxe sql Ssql = preg_replace(sql_regcasec/(fromiselectlinsertideletelwhereldrop tableishow tablesi#R*1-1%\)/17,$sql); Ssql = trim(Ssql);//limpa espacos vazio Ssql = strip_tags(Ssql);//tira tags html e php Ssql = addslashes(Ssql);//Adiciona barras invertidas a uma string return Ssql; } 
-//modo de usar pegando dados vindos do formulario Snome = anti_injection(LPOSTrnomel); Ssenha = anti_injection(S_POSTflenhal); 
+<?php
+
+// Anti Sqllnjection e tags html PHP 
+function anti_injection(Ssql) { 
+    Ssql = preg_replace(preg_replace(sql_regcase("/(from|select|insert|delete|where|drop table|show tables|#|*|--|\\\\)/"),"",$Variavel);); // remove palavras que contenham sintaxe sql 
+    Ssql = trim(Ssql);//limpa espacos vazio 
+    Ssql = strip_tags(Ssql);    //tira tags html e php 
+    Ssql = addslashes(Ssql);//Adiciona barras invertidas a uma string return Ssql; 
+} 
+// modo de usar pegando dados vindos do formulario 
+Snome = anti_injection($_POST['nome']); 
+$senha = anti_injection(S_POST['senha']); 
+
 
